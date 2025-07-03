972c5682005d661b9a63d91958eecd903b2d045f
AnanduDinesan <anandudinesan18@gmail.com>
2025-07-03 15:01:31 +0530

    RequestPage Commit

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4309da6a4dcd23c5d54e20fc21dc000d3ccc8f1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd54df9843dcb96a9733796a245385282a3cca91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
